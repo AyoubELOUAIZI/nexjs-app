0fa9898c6578f7054f4eaca50cf7587bd9a11909
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-08-02 17:09:50 +0100

    navbar styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 
 const Footer = () => {
-  console.log('this is a test')
   return (
     <div>
        <div>©2023 Lamamia. All rights reserved.</div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
 
 
 const links = [
@@ -38,15 +39,15 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">lamamia</Link>
-  <div>
-    {links.map((link) =>(
-    <Link key={link.id} href={link.url}>{link.title}</Link>
-      ))}
-  <button onClick={()=>{
-    console.log('loged out');
-  }}>Logout</button>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo} >Sousarg</Link>
+    <div  className={styles.links}>
+      {links.map((link) =>(
+      <Link className={styles.link} key={link.id} href={link.url}>{link.title}</Link>
+        ))}
+      <button className={styles.logout} onClick={()=>{
+      console.log('loged out');
+      }}>Logout</button>
     </div>
   </div>
   )
