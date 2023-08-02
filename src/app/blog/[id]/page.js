e06e00a126aa13123a285e8e694db4afde28df78
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-08-02 16:28:02 +0100

    first commit

---
+++ src/app/blog/[id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPoste = () => {
+  return (
+    <div>BlogPoste</div>
+  )
+}
+
+export default BlogPoste
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+
+const Footer = () => {
+  console.log('this is a test')
+  return (
+    <div>
+       <div>©2023 Lamamia. All rights reserved.</div>
+       <div>
+        
+       </div>
+    </div>
+   
+    )
+}
+
+export default Footer
