c3e366d871a0c1d28a47a1e7e858bc661c36c7f9
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-08-02 19:56:54 +0100

    adding icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,19 @@
 "use client"
 import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
-       <div>©2023 Lamamia. All rights reserved.</div>
-       <div>
-        
+    <div className={styles.container}>
+       <div>©2023 sousarg. All rights reserved.</div>
+       <div className={styles.social}>
+       <Image  className={styles.icon} src="/1.png" width={15} height={15} alt="facebook image"/>
+       <Image  className={styles.icon} src="/2.png" width={15} height={15} alt="instagram image"/>
+       <Image  className={styles.icon} src="/3.png" width={15} height={15} alt="twiter image"/>
+       <Image  className={styles.icon} src="/4.png" width={15} height={15} alt="youtube image"/>
        </div>
     </div>
-   
     )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const links = [
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.logo} >Sousarg</Link>
+      <Link href="/" className={styles.logo} >sousarg</Link>
     <div  className={styles.links}>
       {links.map((link) =>(
       <Link className={styles.link} key={link.id} href={link.url}>{link.title}</Link>
