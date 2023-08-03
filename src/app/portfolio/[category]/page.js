397ceaa20d127d73eb37652a714075b92c649215
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-08-04 00:09:14 +0100

    portfolio

--- src/app/portfolio/[category]/page.js
+++ src/app/portfolio/[category]/page.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
 
-const Category = () => {
+const getData = (cat) => {
+  // in this case the cat will be "illustrations" or "websites" or "applications" 
+  // because that is what we have in the links in portfolio/page.jsx
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
+const Category = ({ params }) => {
+  // we are puting .category because we have [category] it is like when we have /:id in the params in node.js
+  const data = getData(params.category);
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
 
-export default Category
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=""
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/utils/db.js
@@ -0,0 +1,1 @@
+// in this file we import mongoose and we connect to mongodb database
